0fe51f1591abca2b0f193faf86730acdc6cb488a
mumbi <mumbi099@gmail.com>
2023-04-29 04:31:28 +0300

    Fetch art items & search by category

--- src/main.js
+++ src/main.js
@@ -1,3 +1,84 @@
+fetch('https://my-json-server.typicode.com/mumbi-prog/ArtHive-project/categories')
+  .then(response => response.json())
+  .then(data => {
+    const gallery = document.getElementById('gallery');
+    let filteredData = data;
+
+    const renderGallery = () => {
+      gallery.innerHTML = '';
+
+      filteredData.forEach(category => {
+        const categoryDiv = document.createElement('div');
+        categoryDiv.className = 'art-category';
+
+        const categoryImg = document.createElement('img');
+        categoryImg.src = category.image;
+        categoryImg.alt = category.name;
+        categoryDiv.appendChild(categoryImg);
+
+        const categoryName = document.createElement('h3');
+        categoryName.textContent = category.name;
+        categoryDiv.appendChild(categoryName);
+
+        category.artItems.forEach(item => {
+          const itemDiv = document.createElement('div');
+          itemDiv.className = 'art-item';
+
+          const itemImg = document.createElement('img');
+          itemImg.src = item.image;
+          itemImg.alt = item.description;
+          itemDiv.appendChild(itemImg);
+
+          const itemDesc = document.createElement('p');
+          itemDesc.textContent = item.description;
+          itemDiv.appendChild(itemDesc);
+
+          const itemArtist = document.createElement('p');
+          itemArtist.textContent = `Artist: ${item.artist}`;
+          itemDiv.appendChild(itemArtist);
+
+          const itemPrice = document.createElement('p');
+          itemPrice.textContent = `Price: Ksh. ${item.price}`;
+          itemDiv.appendChild(itemPrice);
+
+          categoryDiv.appendChild(itemDiv);
+        });
+
+        gallery.appendChild(categoryDiv);
+      });
+    };
+
+    renderGallery();
+
+    const searchInput = document.getElementById('search-input');
+    const searchButton = document.getElementById('search-button');
+
+    searchButton.addEventListener('click', () => {
+      const searchValue = searchInput.value.toLowerCase().trim();
+      filteredData = data.filter(category => category.name.toLowerCase().includes(searchValue));
+      renderGallery();
+    });
+  })
+  .catch(error => console.error(error));
+
+//------------------------------------------------------------------------------------------//
+
+// Function to display the reviews
+fetch('https://my-json-server.typicode.com/mumbi-prog/ArtHive-project/reviews')
+.then(response => response.json())
+.then(data => {
+  const reviewsList = document.getElementById('reviews-list');
+  data.forEach(review => {
+    const li = document.createElement('li');
+    li.innerHTML = `<h3 class="review-name">${review.name} - ${review.rating}</h3><p class="review-comment">${review.comment}</p>`;
+    reviewsList.appendChild(li);
+  });
+})
+.catch(error => console.error(error));
+
+//------------------------------------------------------------------------------------------//
+
+// Function to submit the form
 function submitForm(event) {
   // Prevent default form submission behavior
   event.preventDefault();
@@ -12,44 +93,21 @@ function submitForm(event) {
   const phoneValue = phoneInput.value;
   const messageValue = messageInput.value;
 
-  // Reset the input field values
-  nameInput.value = '';
-  emailInput.value = '';
-  phoneInput.value = '';
-  messageInput.value = '';
-}
-
   // Display success message
   alert('Message sent!');
+
+    // Reset the input field values
+    nameInput.value = '';
+    emailInput.value = '';
+    phoneInput.value = '';
+    messageInput.value = '';
+  }
 
 // Add event listener to submit button
 const submitBtn = document.getElementById('submit-btn');
 submitBtn.addEventListener('click', submitForm);
 
-// Function to display the reviews
-fetch('https://my-json-server.typicode.com/mumbi-prog/ArtHive-project/reviews')
-.then(response => response.json())
-.then(data => {
-  const reviewsList = document.getElementById('reviews-list');
-  data.forEach(review => {
-    const li = document.createElement('li');
-    li.innerHTML = `<h3 class="review-name">${review.name} - ${review.rating}</h3><p class="review-comment">${review.comment}</p>`;
-    reviewsList.appendChild(li);
-  });
-})
-.catch(error => console.error(error));
 
-// Function to display the reviews
-fetch('https://my-json-server.typicode.com/mumbi-prog/ArtHive-project/reviews')
-.then(response => response.json())
-.then(data => {
-  const reviewsList = document.getElementById('reviews-list');
-  data.forEach(review => {
-    const li = document.createElement('li');
-    li.innerHTML = `<h3 class="review-name">${review.name} - ${review.rating}</h3><p class="review-comment">${review.comment}</p>`;
-    reviewsList.appendChild(li);
-  });
-})
-.catch(error => console.error(error));
+
 
 
